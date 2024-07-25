14361bc528344c7b4124d0013d2a6fd54bf72e19
pa3ulovictor <p_victor89@hotmail.com>
2024-07-25 20:49:29 -0300

    Modificação na formatação do corpo da tabuada utilizando o .format

--- tabuada.py
+++ tabuada.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
 ...
-_________________
+#################
 
-Tabuada do 2
-2
-4
+---Tabuada do 2---
+    2 x 1 = 2
+    2 x 2 = 4
 ...
-_________________
+################# 
 
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Paulo Victor"
 
-#  numeros = [1,2,3,4,5,6,7,8,9,10]
-numeros =list(range(1, 11))
 
+#  numeros = [1,2,3,4,5,6,7,8,9,10]
 # Iterable (pecorriveis)
 
+numeros =list(range(1, 11))
+
+
 # para cada numero em numeros
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("____________")
+for n1 in numeros:
+    print("{:-^18}".format("Tabuada"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print()    
 
