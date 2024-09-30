4913335ea09a2f37c4424c5da7c9d09c571413ad
pa3ulovictor <p_victor89@hotmail.com>
2024-09-30 20:16:45 -0300

    Atualização de versão e refatorando Hello.py

--- hello.py
+++ hello.py
@@ -15,21 +15,31 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Paulo Victor"
 __licinse__ = "Unlicense"
 
 import os 
 
-current_language = os.getenv ("LANG") [:5]
+current_language = os.getenv ("LANG", "en_US") [:5]
 # Aqui pegamos a linguagem até o caracter 5 
-msg = "Hello, World!"
+msg = {
+        "en_US" : "Hello world",
+        "pt_BR" : "Olá, Mundo!",
+        "it_IT" : "Cioa Mondo",
+        "es_SP" : " Hola, Mundo",
+        "fr_FR" : "Bonjour MOnde",
+        }
+#Ordem de complexidade O(n)
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
+#if current_language == "pt_BR":
+ #   msg = "Olá, Mundo!"
+#elif current_language == "it_IT":
+ #   msg = "Ciao, Mondo!"
+#elif current_language == "es_SP":
+#    msg = "Hola, Mundo!"
+#elif current_language == "fr_FR":
+#    msg = "Bonjour Monde"
     
-printf(msg)
+print(msg[current_language])
+
